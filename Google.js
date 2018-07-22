01e318b2d719c826b273c9e3d5aab45e22b421d1
Linkcariloker <32061424+linkloker@users.noreply.github.com>
2018-07-23 04:45:35 +0700

    Update Google.js

--- Google.js
+++ Google.js
@@ -1,1 +1,9 @@
-
+<div id='google_translate_element'/><script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: &#39;id&#39;,
+    multilanguagePage: true,
+    layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+  }, &#39;google_translate_element&#39;);
+}
+</script><script src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'/>
